a9693048a1e3cefb880e2efb54fedd1a37022205
ariTech <Lahiri.ari@outlook.com>
2021-07-05 08:33:22 +0800

    insertion sort theorty add

--- insertionSort.js
+++ insertionSort.js
@@ -1,5 +1,13 @@
 // insertion sort
 
+/*
+
+1: Iterate from arr[1] to arr[n] over the array. 
+2: Compare the current element (key) to its predecessor. 
+3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element.
+
+*/
+
 const insertionSort = arr => {
     const len = arr.length;
     for (let i = 0; i < len; i++) {
